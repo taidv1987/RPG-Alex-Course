fea3a10981f6b54148a7eee74e2866401a5ccdfc
dvtai19871 <dvtai1987@gmail.com>
2023-08-02 23:17:27 +0700

    Up to lesson 17. Jump Animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,8 +13,15 @@ public class Player : MonoBehaviour
     [SerializeField][Range(1f, 20f)] private float moveSpeed;
     [SerializeField][Range(1f, 20f)] private float jumpForce;
 
-    [SerializeField] private bool isMoving;
     private float xInput;
+
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+    [Header("Collision Info")]
+    [SerializeField] float distanceToGround;
+    [SerializeField] LayerMask setGroundLayer;
+    private bool isGrounded;
 
     void Start()
     {
@@ -23,16 +31,72 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        KeyboardInput();
+        Movement();
+
+        CollisionCheck();
+
+        FlipController();
+        AnimatorControllers();
+    }
+
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, distanceToGround, setGroundLayer);
+    }
+
+    private void KeyboardInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (xInput * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if (isGrounded)
+                Jump();
         }
+    }
 
-        isMoving = rb.velocity.x != 0;
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
         animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        } 
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - distanceToGround));
     }
 }
