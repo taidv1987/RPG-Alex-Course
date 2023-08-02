d73d2c8c31b6982cdfbad46d88dc2bc15ea5336a
DESKTOP-IHG3MH9\TaiDo <tai.dv@dgwasia.com>
2023-08-02 17:21:38 +0700

    Up to lesson 13. Clean Up

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,33 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed;
-    public float jumpForce;
+    private Rigidbody2D rb;
+    private Animator animator;
 
+    [SerializeField][Range(1f, 20f)] private float moveSpeed;
+    [SerializeField][Range(1f, 20f)] private float jumpForce;
+
+    [SerializeField] private bool isMoving;
     private float xInput;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-
         xInput = Input.GetAxisRaw("Horizontal");
-
         rb.velocity = new Vector2 (xInput * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        isMoving = rb.velocity.x != 0;
+
+        animator.SetBool("isMoving", isMoving);
     }
 }
