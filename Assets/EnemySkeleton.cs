f0c88059bf993655686e34bb503cc97daf7b85c8
dvtai19871 <dvtai1987@gmail.com>
2023-08-07 21:20:29 +0700

    Up to lesson 26.Enemy's attack

--- Assets/EnemySkeleton.cs
+++ Assets/EnemySkeleton.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySkeleton : Entity
 {
+    bool isAttacking;
+
     [Header("Move info")]
     [SerializeField] private float moveSpeed;
 
+    [Header("Player detection")]
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private LayerMask playerLayer;
+    private RaycastHit2D isPlayerDetected;
 
     protected override void Start()
     {
@@ -17,11 +21,50 @@ public class EnemySkeleton : Entity
     {
         base.Update();
 
+        if (isPlayerDetected)
+        {
+            if (isPlayerDetected.distance > 1)
+            {
+                rb.velocity = new Vector2(facingDir * moveSpeed * 3f, rb.velocity.y);
+                isAttacking = false;
+
+                Debug.Log("Warning! see the player");
+            }
+            else
+            {
+                Debug.Log("Attack" + isPlayerDetected.collider.gameObject.name);
+                isAttacking = true;
+            }
+        }
+
         if (!isGrounded || isWallDetected)
         {
             Flip();
         }
 
-        rb.velocity = new Vector2 (facingDir * moveSpeed, rb.velocity.y);
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (!isAttacking)
+        {
+            rb.velocity = new Vector2(facingDir * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    protected override void CollisionCheck()
+    {
+        base.CollisionCheck();
+
+        isPlayerDetected = Physics2D.Raycast(transform.position, Vector2.right, distanceToPlayer * facingDir, playerLayer);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + distanceToPlayer * facingDir, transform.position.y));
     }
 }
